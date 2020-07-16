c5ef568af890e7a280a02cbffb806db05a1e8ffb
Landon <reasoner.landon90@gmail.com>
2020-07-16 01:58:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,6 @@ def is_prime? (number)
   return false if number.integer? == false
   return false if number < 2
   return true if number == 2
-  (2..number-1).each {|x|return false if number % == 0}
+  (2..number-1).each {|x|return false if number % x == 0}
   true
 end
